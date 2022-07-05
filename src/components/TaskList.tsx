864014b9598d825106c195e94b74408f4e36f408
AurelioAquino92 <aurelio.obmep@gmail.com>
2022-07-04 20:33:53 -0400

    my solution

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,27 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle == '') return
+
+    const newTask = {
+      id: tasks.length > 0 ? Math.max(...tasks.map(task => task.id)) + 1 : 1,
+      title: newTaskTitle,
+      isComplete: false
+    }
+    setTasks([...tasks, newTask])
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    let newTasks = tasks.slice()
+    const taskIdx = tasks.findIndex(task => task.id == id)
+    newTasks[taskIdx].isComplete = !newTasks[taskIdx].isComplete
+    setTasks(newTasks)
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    setTasks(tasks.filter(task => task.id != id))
   }
 
   return (
